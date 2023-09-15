3a7622c66d725983d1ff42c5b98b809e6a81d9a8
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-15 23:35:50 +0530

    Clean the project

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 console.log("App ran");
-const fs = require("fs/promises");
-
-setInterval(async () => {
-  const message = new Date().toLocaleTimeString().replaceAll(" ", "-");
-  fs.writeFile(`${message}.txt`, "I am on " + message, (err) =>
-    console.log(err)
-  );
-}, 100);
---
+++ utils/_test_scripts.js
@@ -0,0 +1,8 @@
+const fs = require("fs/promises");
+
+setInterval(async () => {
+  const message = new Date().toLocaleTimeString().replaceAll(" ", "-");
+  fs.writeFile(`${message}.txt`, "I am on " + message, (err) =>
+    console.log(err)
+  );
+}, 100);
