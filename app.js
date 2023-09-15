0ec548e10b4bf17c1c888578520e58d9829c62ff
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-16 02:04:11 +0530

    Add a simple starter server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("App ran");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("Server is running fine");
+});
+
+app.use((req, res) => {
+  res.send("404");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Backend server started on port ${PORT}`);
+});
