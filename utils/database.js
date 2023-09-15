6fed67f1f407d1c8c9a471b45fba819e4ec493dc
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-16 02:02:53 +0530

    Add connection pooling

--- utils/database.js
+++ utils/database.js
@@ -2,21 +2,51 @@ const mongoose = require("mongoose");
 const ENV = require("./getEnv");
 const database_password = ENV["MONGODB_PASSWORD"];
 
-function connectToDBAndRun(cb = () => {}) {
-  mongoose
-    .connect(
-      `mongodb+srv://sanjarcode-nodejscompleteguide:${database_password}@cluster-nodejscompleteg.nuohpop.mongodb.net/?retryWrites=true&w=majority`
-    )
-    .then((success) => {
-      console.log("Connected to MongoDB Atlas");
-      //   console.log({ success });
-      cb();
-    })
-    .catch((error) => {
-      console.log("Mongo DB connection failed");
-      //   console.log({ err: error });
-    });
-}
-// connectToDBAndRun();
+// connection + pooling inspired by https://github.com/exemplar-codes/online-shop-with-nosql-mongoose/blob/843745fbe8dc144b492257ac130b1a1cb72bed75/util/database.js
 
-module.exports = connectToDBAndRun;
+let _db = null;
+
+/**
+ * Establishes connection to the database.
+ * Updates connection pool even if it exists.
+ */
+async function connectToDB() {
+  try {
+    const mongooseObject = await mongoose.connect(
+      `mongodb+srv://sanjarcode-nodejscompleteguide:${database_password}@cluster-nodejscompleteg.nuohpop.mongodb.net/?retryWrites=true&w=majority`
+    );
+    console.log("Connected to MongoDB Atlas");
+    // console.log({ success: mongooseObject });
+
+    const mongoDbClient = mongooseObject.connection.getClient();
+    _db = mongoDbClient.db();
+  } catch (error) {
+    console.log("Mongo DB connection failed");
+    //   console.log({ err: error });
+    throw error;
+  }
+
+  return _db;
+}
+// connectToDB();
+
+/**
+ * Gets a 'connection' from existing pool.
+ * Throws an error if pool is empty.
+ *
+ * @param {Boolean} retry, retry a connection if pool is empty
+ */
+async function getDB(retry = false) {
+  if (_db) {
+    return _db;
+  }
+
+  if (retry) {
+    await connectToDB();
+    return db;
+  }
+
+  throw "No database connection found!";
+}
+
+module.exports = { connect: connectToDB, get: getDB };
