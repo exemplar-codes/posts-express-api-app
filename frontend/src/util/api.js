6bd2c262013388b1da300e680779adc10fd737a7
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-17 03:44:30 +0530

    Add API prefix for SPA/React dev mode

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,1 +1,16 @@
-export const postsURL = "/posts";
+function getDevelopmentAPIUrlPrefix() {
+  const domain = window.location.origin;
+  const cleanedDomain = domain
+    .split(":")
+    .slice(0, -1)
+    .join(":");
+  const port = process.env.PORT || 8080;
+  const url = `${cleanedDomain}:${port}`;
+
+  return url;
+}
+
+export const postsURL =
+  process.env.NODE_ENV === "development"
+    ? `${getDevelopmentAPIUrlPrefix()}/posts`
+    : "/posts";
