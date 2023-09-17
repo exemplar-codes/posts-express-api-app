385d7846b04e7ed9447a709b9d8df0eb7e40f10a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-17 06:00:08 +0530

    Re-add dump build folder for safety

---
+++ frontend/build/precache-manifest.43ef1a7cef880793f98d5da925f5338d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "082d6bc36abe2dc04eef",
+    "url": "/static/css/main.54f090de.chunk.css"
+  },
+  {
+    "revision": "082d6bc36abe2dc04eef",
+    "url": "/static/js/main.082d6bc3.chunk.js"
+  },
+  {
+    "revision": "bc47c2e03f23eb0d1e1a",
+    "url": "/static/js/1.bc47c2e0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6867637ad5a871cb354930a553c7b019",
+    "url": "/index.html"
+  }
+];
