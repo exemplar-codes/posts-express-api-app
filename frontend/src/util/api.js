abd00a755defcc39020494610231ae6d734dfb52
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-09-17 23:12:57 +0530

    Add env variables in Vite, equivalent

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,16 +1,12 @@
 function getDevelopmentAPIUrlPrefix() {
   const domain = window.location.origin;
-  const cleanedDomain = domain
-    .split(":")
-    .slice(0, -1)
-    .join(":");
-  const port = process.env.PORT || 8080;
+  const cleanedDomain = domain.split(":").slice(0, -1).join(":");
+  const port = import.meta.env.PORT || 8080;
   const url = `${cleanedDomain}:${port}`;
 
   return url;
 }
 
-export const postsURL =
-  process.env.NODE_ENV === "development"
-    ? `${getDevelopmentAPIUrlPrefix()}/posts`
-    : "/posts";
+export const postsURL = import.meta.env.DEV
+  ? `${getDevelopmentAPIUrlPrefix()}/posts`
+  : "/posts";
